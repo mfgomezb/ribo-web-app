a4f677d6ad5494ecaa8338dfd2c02569ed479aae
Miguel Gomez <mgomezb89@gmail.com>
2021-03-09 18:37:41 +0100

    clean up

--- src/layouts/DashboardLayout/index.js
+++ src/layouts/DashboardLayout/index.js
@@ -37,6 +37,7 @@ const useStyles = makeStyles((theme) => ({
 const DashboardLayout = ({ children }) => {
   const classes = useStyles();
   const [isMobileNavOpen, setMobileNavOpen] = useState(false);
+  useLocationOptions()
 
   return (
     <div className={classes.root}>
